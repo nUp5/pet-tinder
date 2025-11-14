d6eee2d762b9ca53953ffc2d21e42299fae55218
nUp5 <nup5@seznam.cz>
2025-11-14 18:33:39 +0100

    Alfa verze, stylování  úvodní stránky

--- app/Models/Swipe.php
+++ app/Models/Swipe.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Swipe extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'posouvajici_id',
+        'posunuty_id',
+        'libi_se',
+    ];
+
+    public function posouvajici()
+    {
+        return $this->belongsTo(Zvire::class, 'posouvajici_id');
+    }
+
+    public function posunuty()
+    {
+        return $this->belongsTo(Zvire::class, 'posunuty_id');
+    }
 }
